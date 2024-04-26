ac8f00808772637399ff09e100c91503ab7eecb8
Washingtone Aura <aurawashingtone@gmail.com>
2024-04-26 15:50:57 +0300

    fixed the js for the toggle menu, checkout the css

---
+++ public/main.js
@@ -0,0 +1,19 @@
+/*const dropdownButton = document.querySelector('[data-dropdown-toggle="dropdownMenu"]');
+const dropdownMenu = document.getElementById('dropdownMenu');
+
+dropdownButton.addEventListener('click', () => {
+  dropdownMenu.classList.toggle('hidden');
+}); */
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    console.log('Dropdown toggle clicked');
+
+    var dropdownToggle = document.querySelector('[data-dropdown-toggle="dropdownMenu"]');
+    var dropdownMenu = document.getElementById('dropdownMenu');
+   
+    dropdownToggle.addEventListener('click', function() {
+       dropdownMenu.classList.toggle('hidden');
+    });
+});
+   
